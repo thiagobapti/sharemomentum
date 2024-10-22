e0bebdfa6d0fbe652b81ca5bd4f685d4f6bdd6f9
Thiago Baptista <thiago@syncmob.com.br>
2024-10-22 13:46:32 -0300

    Update copy

--- src/app/components/unsplash-dialog.tsx
+++ src/app/components/unsplash-dialog.tsx
@@ -71,7 +71,6 @@ function UnsplashDialog({
           <Button colorScheme="blue" mr={3} onClick={() => onClose("")}>
             Close
           </Button>
-          <Button variant="ghost">Secondary Action</Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
