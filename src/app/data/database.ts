0236c00ba6b16a372afaa8e887e3ddbf0d1da1cb
Thiago Baptista <thiago@syncmob.com.br>
2024-10-25 17:32:05 -0300

    Update program context and keywords

--- src/app/data/database.ts
+++ src/app/data/database.ts
@@ -2,20 +2,32 @@ const programs = [
   {
     id: 1,
     name: "The Life You Can Save",
-    context: ["charity", "impact", "giving"],
-    keywords: ["charity", "impact", "giving"],
+    context: [
+      "About Us: We are a community of people who believe in taking action. Our role is to help you make the most effective donations possible in the fight against extreme poverty. Since our founding in 2013, we have compiled a list of research-backed charities working on interventions against the multidimensional factors of poverty, resulting in over $100 million donated to high-impact charities.  ",
+      "Our Vision: Our vision is to live in a world where there is no global poverty.",
+      "Our Mission: Our mission is to improve the lives of people living in poverty by changing the way people think about and donate to charity.",
+      "Cause Area: Poverty, Global Health and Development",
+    ],
+    keywords: ["charity", "donation", "poverty", "global health"],
   },
   {
     id: 2,
     name: "House of Ruth Maryland",
-    context: ["domestic violence", "support", "safety"],
-    keywords: ["domestic violence", "support", "safety"],
+    context: [
+      "About: Empowering Women and Families Since 1976 - House of Ruth is a 501(c)(3) nonprofit in Washington, D.C. We believe that everyone deserves to live a life free from violence and fear. In 1976, we began as a housing-focused organization. As years went, on we came to understand the variety of needs accompanying houselessness and domestic violence. Today, we operate 15 programs across the District of Columbia, including housing, counseling, and child development services. House of Ruth’s ultimate goal is to empower survivors of domestic violence to break the cycle of abuse and live independently, safely, and with dignity. Our organization aims to achieve this by providing comprehensive, trauma-informed services that address the unique needs of each survivor and their family. House of Ruth is committed to serving all survivors of domestic violence, regardless of their race, gender, sexual orientation, religion, or nationality.",
+      "Who we serve: House of Ruth’s services* support the women who live in the greater DC area. Washington, DC, has a history of lacking shelters for women, and our services were built on improving those conditions. House of Ruth is open to anyone who identifies as a woman and their dependents.",
+      "How we serve: Our services include housing, child development and counseling. We believe that survivors need support in many different aspects of their lives in order to empower themselves toward a brighter future. We’re working tirelessly to continue to strengthen our services so that we can end the cycle of violence. Many of our program participants have gone on to purchase homes, pursue graduate degrees and thrive in their careers. All this wouldn’t have been possible without their self-determination and the support we’re able to offer.",
+    ],
+    keywords: ["charity", "donation", "domestic violence"],
   },
   {
     id: 3,
     name: "Koinonia",
-    context: ["poverty", "community", "support"],
-    keywords: ["poverty", "community", "support"],
+    context: [
+      "About Becoming a Koinonia Parent: There is nothing more rewarding in this world than helping a child in need. Being a Koinonia parent means you will be a part of an organization with very high standards and expertise in helping children in need. Children in foster care have been exposed to various forms of trauma and we need you to work with us and help these children. As a Koinonia parent you might be the first person in their life to truly love and care for them.",
+      "About Adoption: Our adoption program assists families with Adoption Through Foster Care, Private Domestic Adoption, and Stepparent Adoption. Koinonia’s biggest priority is to provide a safe environment for children in foster care and prevent youth from entering the system if possible. Koinonia has several services put in place to give the best care to at-risk youth. In the case that children cannot be reunified with their biological families, we also work to find them a home open to adopting them through friends/family close to the youth or our vetted resource families.",
+    ],
+    keywords: ["charity", "donation", "adoption", "family"],
   },
 ];
 
